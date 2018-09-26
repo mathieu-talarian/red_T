fbf34d7a0898b4e49b70db14fa79b796805b9a2e
Mathieu MOULLEC <mmoullec@e1r7p6.42.fr>
2018-09-26 19:19:47 +0200

    pep8 fixer

--- src/client/components/Case.js
+++ src/client/components/Case.js
@@ -2,12 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import Cube from "../assets/cube.svg";
 
-const Case = ({ content }) => <Cube className="case" />;
+const Case = ({ content }) => {
+  const color = content != 0 ? content.className : "";
+  const className = `case ${color}`;
+  return <Cube className={className} />;
+};
 // const Case = ({ content }) => (
 //   <div className={"case" + (content ? " piece" : "")} />
 // );
 Case.propTypes = {
-  content: PropTypes.number.isRequired
+  content: PropTypes.any.isRequired
 };
 
 Case.style = () => ({
--- src/client/components/Content.js
+++ src/client/components/Content.js
@@ -3,13 +3,7 @@ import Board from "./Board";
 
 const Content = () => (
   <div className="content">
-    <div className="top_p" />
-    <div className="center">
-      <div className="p" />
-      <Board />
-      <div className="p" />
-    </div>
-    <div className="bot_p" />
+    <Board />
   </div>
 );
 
